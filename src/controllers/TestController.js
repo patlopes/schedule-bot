7d5de62e1b634c6ec78a5819aa46b4cdbafd14c7
Patrick Lopes <pferreira@realprotect.net>
2020-08-23 16:48:55 -0300

    teste

--- src/controllers/TestController.js
+++ src/controllers/TestController.js
@@ -1,13 +1,18 @@
 module.exports = {
-    async index(request, response) {
+    async index(req, res) {
         return response.json(["isso veio da api :)"]);
     },
 
-    async create(request, response) {
-        console.log(request.body);
-        console.log(request.json);
-        //console.log(request);
-        return response.json(request.body);
+    async create(req, res) {
+        const {user_id, bot_id, module_id} = req.body;
+        const response = {
+            user_id,
+            bot_id,
+            module_id,
+            message: "essa mensagem veio de uma resisição WEB"
+        }
+        console.log(response);
+        return res.json(response);
     }
 }
 
